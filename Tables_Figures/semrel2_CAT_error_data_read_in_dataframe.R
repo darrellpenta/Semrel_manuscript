aa7abec7fae165eafaa5791fbda3e1b0b5afa0fc
Darrell Jonathan Penta <d.penta@neu.edu>
2015-09-05 10:51:57 -0400

    Corrected CAT error rates report
    Missind “errcord” column, so rates were coming up as NaN

--- Tables_Figures/semrel2_CAT_error_data_read_in_dataframe.R
+++ Tables_Figures/semrel2_CAT_error_data_read_in_dataframe.R
@@ -5,7 +5,7 @@ d.base           <- read.csv("data/SR1&2_coding.csv", stringsAsFactors = FALSE )
 d.cat            <- subset(d.base, subexpt == "Cat")
 d.cat            <- subset(d.cat,  subject != "1031")
 d.cat            <- subset(d.cat,  subject != "1123")
-                           
+d.cat$errcord <- (d.cat$errd + d.cat$corrd)
 d.cat$related    <- as.factor(d.cat$related)
 d.cat$related    <- droplevels(d.cat$rel)
 d.cat$n2num      <- as.factor(d.cat$n2num)
