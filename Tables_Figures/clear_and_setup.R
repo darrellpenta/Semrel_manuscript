e60dbc620dff049881fe8e4e6a802402ee2e4600
Darrell Jonathan Penta <d.penta@neu.edu>
2015-09-22 21:16:26 -0400

    Final manuscript sent to Neal
    this has the different DF for the model comparison

--- Tables_Figures/clear_and_setup.R
+++ Tables_Figures/clear_and_setup.R
@@ -11,6 +11,8 @@ options( scipen=1)
 line = rep(c("-"), times = 40, fill = 80)
 br   = "\n"
 
+
+
 get_range = function( p) {
   range = findInterval( p, c(0, 0.001, 0.01, 0.05, 0.1))
   codes = c("<.001" , "<.01","<.05", "<.10", ">.10")
